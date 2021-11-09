38d3c9c3db3247897d85ad451d1a77ff31937b99
Teplov Andrew <teplov.andrew@gmail.com>
2021-11-09 13:48:26 +0300

    ineffective solution ready

--- main.cpp
+++ main.cpp
@@ -8,22 +8,23 @@ void number_of_products_div()
     +  читать и запомнить входные данные
     +  вывести все пары
     +  вместо вывода пары посчиатть ее произведение
-    4. вывести только пары, произведения которых кратно 55
-    5. только количество пар
+    +  вывести только пары, произведения которых кратно 55
+    +  только количество пар
     */
     int n(0);
     cin >> n;
     vector<int> numbers(n, 0);
     for (int i(0); i<n; i++)
         cin>>numbers[i];
-
+    int good_pairs(0);
     for (int j(0); j<n-1; j++)
         for (int i(j+1); i<n;i++)
         {
             int cur_prod(numbers[j]*numbers[i]);
             if (cur_prod%21==0)
+                good_pairs++;
         }
-
+    cout<< good_pairs<< endl;
 }
 
 int main()
