64dcf62571c48b51e960421ddd19393ddeb37c74
Teplov Andrew <teplov.andrew@gmail.com>
2021-11-09 13:37:09 +0300

    prints pairs

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 void number_of_products_div()
 {
     /**
-    1. читать и запомнить входные данные
+    + читать и запомнить входные данные
     2. вывести все пары
     3. вместо вывода пары посчиатть ее произведение
     4. вывести только пары, произведения которых ерптно 55
@@ -16,8 +16,11 @@ void number_of_products_div()
     vector<int> numbers(n, 0);
     for (int i(0); i<n; i++)
         cin>>numbers[i];
-    for (int i(0); i<n; i++)
-        cout<<numbers[i]<<" ";
+
+    for (int j(0); j<n-1; j++)
+        for (int i(j+1); i<n;i++)
+            cout<<numbers[j]<<" "<<numbers[i]<< endl;
+
 }
 
 int main()
