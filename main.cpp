89b18e05890bb27a428a44dc7ef5ff3eca56fe5c
Teplov Andrew <teplov.andrew@gmail.com>
2021-11-16 12:45:39 +0300

    reads and prints numbers

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 void max_sum_div()
@@ -10,6 +11,15 @@ void max_sum_div()
     4. делимость сумм проверить
     5. вывести только наибольшую сумму
     */
+
+    int n(0);
+    cin >> n;
+    vector<int> numbers(n);
+    for (int i(0); i<n; i++)
+        cin >> numbers[i];
+    cout << endl;
+    for (int i(0); i<n; i++)
+        cout << numbers[i] << " ";
 }
 int main()
 {
