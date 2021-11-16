9f8e7429386f8c42e2f5265af0b76d2a6151aafc
Teplov Andrew <teplov.andrew@gmail.com>
2021-11-16 12:54:21 +0300

    read from file, prints pairs

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 void max_sum_div()
 {
+    ifstream ifs("data.txt");
     /**
-    1. считать и сохранять все числа
+    + 1. считать и сохранять все числа
     2. вывести все пары
     3. вместо пары вывести суммы
     4. делимость сумм проверить
@@ -13,13 +15,15 @@ void max_sum_div()
     */
 
     int n(0);
-    cin >> n;
+    ifs >> n;
     vector<int> numbers(n);
     for (int i(0); i<n; i++)
-        cin >> numbers[i];
+        ifs >> numbers[i];
     cout << endl;
-    for (int i(0); i<n; i++)
-        cout << numbers[i] << " ";
+    for (int j(0); j<n-1; j++)
+        for (int i(j+1); i<n;i++)
+            cout<<numbers[j]<<" "<<numbers[i]<< endl;
+
 }
 int main()
 {
