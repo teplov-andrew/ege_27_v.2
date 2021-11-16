ff296fc64700361b1f03d8c51db3020712c9e5e0
Teplov Andrew <teplov.andrew@gmail.com>
2021-11-16 12:37:38 +0300

    added dev plan

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 
 using namespace std;
-
+void max_sum_div()
+{
+    /**
+    1. считать и сохранять все числа
+    2. вывести все пары
+    3. вместо пары вывести суммы
+    4. делимость сумм проверить
+    5. вывести только наибольшую сумму
+    */
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    max_sum_div();
     return 0;
 }
