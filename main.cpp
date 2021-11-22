770d09b32043489c00a374d616e96630dba4bd16
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-11-22 18:14:52 +0300

    only divideable by 30

--- main.cpp
+++ main.cpp
@@ -16,9 +16,13 @@ void number_of_products_div()
     vector<int> numbers(n, 0);
     for (int i(0); i<n; i++)
         cin>>numbers[i];
+
     for (int j(0); j<n-1; j++)
         for (int i(j+1); i<n;i++)
-            cout<<numbers[j] * numbers[i]<< endl;
+        {
+            int cur_prod(numbers[j]*numbers[i]);
+            if (cur_prod%30==0)
+        }
 }
 
 int main()
