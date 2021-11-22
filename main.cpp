b0f6ecd5ae6de49a85b84742c7e8e47a0d86640e
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-11-22 18:20:27 +0300

    ineffective solution ready

--- main.cpp
+++ main.cpp
@@ -5,24 +5,26 @@ using namespace std;
 void number_of_products_div()
 {
     /**
-    + читать и запомнить входные данные
-    + вывести все пары
-    + вместо вывода пары посчиатть ее произведение
-    4. вывести только пары, произведения которых кратно 30
-    5. только количество пар
+    +  читать и запомнить входные данные
+    +  вывести все пары
+    +  вместо вывода пары посчиатть ее произведение
+    +  вывести только пары, произведения которых кратно 30
+    +  только количество пар
     */
     int n(0);
     cin >> n;
     vector<int> numbers(n, 0);
     for (int i(0); i<n; i++)
         cin>>numbers[i];
-
+    int good_pairs(0);
     for (int j(0); j<n-1; j++)
         for (int i(j+1); i<n;i++)
         {
             int cur_prod(numbers[j]*numbers[i]);
             if (cur_prod%30==0)
+                good_pairs++;
         }
+    cout<< good_pairs<< endl;
 }
 
 int main()
