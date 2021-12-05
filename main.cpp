9e03955c66faa2f2ca83be467d13cfe7573d22d8
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-12-05 16:56:22 +0300

    ineffective solution ready

--- main.cpp
+++ main.cpp
@@ -13,16 +13,17 @@ void min_sum_min_distance_INEFF()
     5. вместо вывода пары - сумму
     6. найти минимальную сумму
     */
-    int n(0), res(0);
+    int n(0);
+	int res(1000);
     cin >> n;
     vector<int> numbers(n, 0);
     for (int i(0); i<n; i++)
         cin>>numbers[i];
     for (int j(0); j<n-4; j++)
         for (int i(j+4); i<n;i++)
-            cout<<numbers[j]+numbers[i]<< endl;
-            if ((numbers[j]+numbers[i])<(numbers[j+1]+numbers[i+1])):
-                res = numbers[j]+numbers[i]
+            if (numbers[j]+numbers[i]<res)
+				res = numbers[i]+numbers[j];
+
 
     cout<<res<< endl;
 
