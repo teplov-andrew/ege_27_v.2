6780d5e48e911604c3a5dccbefc33d31bd967507
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-12-13 21:35:09 +0300

    prints numbers

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
+#include <stdio.h>
 
-using namespace std;
+
+
+void min_sum_distance_ineff_2()
+{
+  // int n = 0;
+  // scanf("%i", &n);
+  int k = 0;
+  int cnt = 0;
+  long int num[9999];
+  while (num[cnt-1] != -9999){
+    scanf("%li", &num[cnt]);
+    cnt++;
+  }
+
+  for(int i=0; i<cnt-1; i++) {
+    printf("%li\n", num[i]);
+  } 
+}
+
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    min_sum_distance_ineff_2();
     return 0;
 }
+
+
+
