f5afe3f301a7d678de94995e57c077eed6b6c186
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-12-14 20:27:43 +0300

    find construction from internet (FindMaxElements in vector)

--- main.cpp
+++ main.cpp
@@ -11,11 +11,39 @@ void number_of_products_div()
     4. делимость сумм проверить
     5. вывести только наибольшую сумму
     */
-	int n(0);
+	std::vector<std::size_t> FindMaxElements(std::vector<int> const& v)
+	{
+    std::vector<std::size_t> indices;
+    int current_max = 0;
+    int prev_max = 0;
+
+    for (std::size_t i = 0; i < v.size(); ++i)
+    {
+        if (v[i] > current_max and prev_max == 0)
+        {
+            prev_max = v[i];
+            current_max = v[i];
+        }
+
+        if (v[i] > current_max and prev_max > 0 and (current_max + v[i]) % 2 != 0)
+        {
+            prev_max = current_max;
+            current_max = v[i];
+            indices.clear();
+            indices.push_back(current_max);
+            indices.push_back(prev_max);
+        }
+
+    }
+
+    return indices;
+	}
+
+    int n(0);
     int res(0);
     cin >> n;
     vector<int> numbers(n);
-    vector<int> res_vector(n)
+    vector<int> res_vector(n);
     for (int i(0); i<n; i++)
         cin >> numbers[i];
 
