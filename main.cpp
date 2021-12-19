7d1f7b8246aff54aa7b9511a87c7facac0203402
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-12-19 17:00:41 +0300

    prints sum of pairs

--- main.cpp
+++ main.cpp
@@ -16,6 +16,13 @@ void num_sum_multiple_distance_ineff_3()
     vector<int> numbers(n, 0);
     for (int i(0); i<n; i++)
         cin>>numbers[i];
+    int good_pairs(0);
+    for (int j(0); j<n-1; j++)
+        for (int i(j+1); i<n;i++)
+        {
+            int cur_sum(numbers[j]+numbers[i]);
+			cout<<cur_sum<<endl;
+		}
 }
 
 int main()
