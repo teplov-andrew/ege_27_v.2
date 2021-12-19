774575386182475a1246e53c46a37cf7f1fd7c4b
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-12-19 16:38:57 +0300

    added dev plan

--- main.cpp
+++ main.cpp
@@ -2,13 +2,19 @@
 
 using namespace std;
 
-void number_of_products_div()
+void num_sum_min_distance_ineff_2()
 {
-    cout << "Hello world!" << endl;
+    /**
+    1  читать и запомнить входные данные
+    2  вывести все пары
+    3  вместо вывода пары посчитать их сумму
+    4  вывести только пары, сумма которых кратно 27 и  разность индексов не менее 4
+    5  только количество пар
+    */
 }
 
 int main()
 {
-    number_of_products_div();
+    num_sum_min_distance_ineff_2();
     return 0;
 }
