6bc50a0bd63e39c2e2a3666f9cf193283e8a69df
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-12-19 16:24:36 +0300

    ineffective solution ready

--- main.cpp
+++ main.cpp
@@ -13,16 +13,20 @@ void num_sum_max_distance_ineff_1()
 	*/
 	int n(0);
     cin >> n;
+	int good_pairs(0);
     vector<int> numbers(n, 0);
     for (int i(0); i<n; i++)
         cin>>numbers[i];
 	
 	for (int j(0); j<n-1; j++)
-    for (int i(j+1); i<n;i++)
-    {
-        int cur_sum(numbers[j]+numbers[i]);
-		cout<<cur_sum<<endl;
-	}
+		for (int i(j+1); i<n;i++)
+		{
+			int cur_sum(numbers[j]+numbers[i]);
+			cout<<cur_sum<<endl;
+			if (cur_sum%39==0 and i-j<7)
+				good_pairs++;
+		}
+	cout<< good_pairs<< endl;
 }	
 
 int main()
