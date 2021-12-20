6b7246510c5a2dcf1f9b143ad7e6311e9b68d755
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-12-20 22:59:30 +0300

    count first 7 numbers

--- main.cpp
+++ main.cpp
@@ -9,6 +9,15 @@ void num_sum_max_distance_eff_1()
     2) просматриваем их на предмет того, кратны ли они 39
     3) берем следующие числа от 7 до конца и поочередно проверяем
     */
+	int n(0);
+    cin >> n;
+    int cnt(0);
+    vector<int> number(7, 0); 
+
+    for (int i(0); i < 7; i++)
+    {
+        cin >> number[i];
+    }
 }
 
 int main()
