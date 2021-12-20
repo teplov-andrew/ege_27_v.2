9d4dfc35eb1ac0dc22392db832f546de8eba1169
teplov-andrew <69726878+teplov-andrew@users.noreply.github.com>
2021-12-20 18:48:55 +0300

    counted numbers and counted the number of elements

--- main.cpp
+++ main.cpp
@@ -12,6 +12,16 @@ void number_sum_div_ineff_2()
 	3. проверить делимость суммы на 40 и наличие элемента больше 40 в паре
 	4. посчитать и вывести итоговое количество 
 	*/
+  int n(0);
+  int cur(0);
+  int cnt(0);
+  vector<int> numbers;
+  while (n!=-9999)
+  {
+    cin >>n;
+    numbers.push_back(n);
+    cur++;
+  }
 }
 
 
