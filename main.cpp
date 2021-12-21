3f56265d2f48cd5b808c271567a6986db49e6c99
Teplov Andrew <teplov.andrew@gmail.com>
2021-12-21 12:36:37 +0300

    added dev plan

--- main.cpp
+++ main.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 
-void number_of_products_div()
+void num_sum_itog_ineff()
 {
-    cout << "Hello world!" << endl;
+    /*
+    1. считать все числа и вывести их
+    2. создать тройки всех числе и вывести их
+    3. вывести только те тройки, у которых сумма чисел делится на 4
+    4. найти минимальную тройку
+    */
 }
 
 int main()
 {
-    number_of_products_div();
+    num_sum_itog_ineff();
     return 0;
 }
