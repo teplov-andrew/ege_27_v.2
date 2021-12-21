87b5ca900e50ae9fefdf1dfac1de8809bbed0be8
Teplov Andrew <teplov.andrew@gmail.com>
2021-12-21 13:59:44 +0300

    almost everything is ready

--- main.cpp
+++ main.cpp
@@ -14,6 +14,9 @@ void num_sum_itog_ineff()
     int n(0);
     cin >> n;
     vector<int> numbers(n,0);
+    int min_sum(4);
+    int cnt(0);
+    int res(1001);
     for (int i(0); i<n; i++)
     {
         cin>>numbers[i];
@@ -22,11 +25,22 @@ void num_sum_itog_ineff()
         for (int i(j+1); i<n-1;i++)
             for (int a(i+1); a<n;a++)
             {
-                if ((numbers[i]+numbers[j]+numbers[a])%4==0)
+                if ((numbers[i]+numbers[j]+numbers[a])%4==0 and (numbers[i]+numbers[j]+numbers[a])<res)
+                {
+                   // cout<<numbers[j]<<" "<<numbers[i]<<" "<<numbers[a]<<" "<< endl;
+                    res=numbers[i]+numbers[j]+numbers[a];
+                    cnt++;
+
+
+                }
+
+                if ((numbers[i]+numbers[j]+numbers[a])==res)
                     cout<<numbers[j]<<" "<<numbers[i]<<" "<<numbers[a]<<" "<< endl;
             }
 
 
+    cout<<res<<endl;
+    cout<<cnt<<endl;
 
 }
 
